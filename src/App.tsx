4564f9a896811304b99a4975b7cd63d78fa39905
mpunga <emmanuelmpunga.23@gmail.com>
2025-12-11 21:14:18 +0100

    ajoute les classes materializ

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent, useState, useEffect } from 'react';
 import Pokemon from './model/pokemon';
 import { POKEMONS } from './model/mock_pokemon';
-import './App.css';
+//import './App.css';
 
 const App: FunctionComponent = () => {
     const [pokemons, setPokemons] = useState<Pokemon[]>([]);
@@ -11,6 +11,32 @@ const App: FunctionComponent = () => {
     }, []);
     
     return (
+        <div>
+            <h1 className="center">Pokédex</h1>
+            <div className="container">
+                <div className="row">
+                    {pokemons.map(({ id, name, picture, created }) => (
+                        <div className="col s6 m4 " key={id}>
+                            <div className="card horizontal">
+                                <div className="card-image">
+                                    <img src={picture} alt={name} />
+                                    </div>
+                                <div className="card-stacked">
+                                    <div className="card-content">
+                                        <p>{name}</p>
+                                        <p><small>{created.toString()}</small></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+        </div>
+    )
+
+    /* return (
         <div className="app-container">
             <h1>Liste de Pokémon</h1>
             <ul className="pokemon-list">    
@@ -21,6 +47,7 @@ const App: FunctionComponent = () => {
                         <div className="pokemon-info">
                             <p><strong>HP:</strong> <span>{pokemon.hp}</span></p>
                             <p><strong>CP:</strong> <span>{pokemon.cp}</span></p>
+                            <button className="delete-button">Supprimer</button>
                             <div className="pokemon-types">
                                 {pokemon.types.map((type, idx) => (
                                     <span key={idx} className="type-badge">{type}</span>
@@ -31,7 +58,7 @@ const App: FunctionComponent = () => {
                 ))}
             </ul>
         </div>  
-    );
+    );  */
 }
   
 export default App; 
